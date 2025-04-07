02e2154c34e214dc62d48753286368473fc5cebd
samvrny <samvrny@gmail.com>
2025-04-06 22:47:47 -0500

    added some initial style and such to the nav/header just to test out the look and feel

---
+++ uisandbox/uisandbox/src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+
+export default function Header() {
+
+    return(
+        <header>
+            <div id="MOCKLOGO">
+                <p>MyCookbook</p>
+            </div>
+
+            <nav>
+                {/* This is probably where the browserrouter components will live... */}
+                {/* For style demos sake... */}
+                <a href="">Page Link</a>
+                <a href="">Page Link</a>
+                <a href="">Page Link</a>
+            </nav>
+        </header>
+    )
+
+}
