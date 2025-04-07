88b566e855a8a17a7978e2a056727d878f12419b
samvrny <samvrny@gmail.com>
2025-04-06 23:03:29 -0500

    mocking up some of the user homepage nav/components

--- uisandbox/uisandbox/src/App.jsx
+++ uisandbox/uisandbox/src/App.jsx
@@ -2,6 +2,7 @@ import './styles/style.css' //IMPORT THE NEW STYLESHEET
 
 //Import Components (for tesing. BrowserRouter will eventually route to pages... I think)
 import Header from './components/Header'
+import UserHome from './components/UserHome'
 
 
 function App() {
@@ -9,6 +10,14 @@ function App() {
   return (
     <>
       <Header />
+
+      <hr />
+      <hr />
+      <p>MOCK USER HOME SCREEN BELOW</p>
+      <hr />
+      <hr />
+
+      <UserHome />
     </>
   )
 }
---
+++ uisandbox/uisandbox/src/components/UserHome.jsx
@@ -0,0 +1,19 @@
+
+export default function UserHome() {
+
+    return (
+        <main id="userHome">
+            <aside>
+                <div className="categoryButton">Breakfast</div>
+                <div className="categoryButton">Lunch</div>
+                <div className="categoryButton">Dinner</div>
+                <div className="categoryButtonGreen">Add Category +</div>
+            </aside>
+
+            <section>
+                <a href="">Create New Recipe</a>
+            </section>
+        </main>
+    )
+
+}
