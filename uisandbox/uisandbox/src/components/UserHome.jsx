6c5c14dbd2517f7648f4965fb2de558b0315151e
samvrny <samvrny@gmail.com>
2025-04-07 22:48:29 -0500

    got quite a good deal of the user home programming completed. Will just really need to scaffold in some style and the code necessary to connect to the actual database and get the data I am using form there rather than a JSON file

--- uisandbox/uisandbox/src/components/UserHome.jsx
+++ uisandbox/uisandbox/src/components/UserHome.jsx
@@ -1,19 +1,30 @@
 import mockData from '../mockRecipeData/mockRecipes.json'
 
+import { useState, useEffect } from 'react';
+
 
 export default function UserHome() {
 
+    //Get categories
     let categories = mockData.categories;
 
+    //Get recipes
+    let recipes = mockData.recipes;
+    // let recipes = [];
+
     // console.log(categories);
+    // console.log(recipes);
+
+    const [currentCategory, setCurrentCategory] = useState("");
+    console.log("Current Category on Render: " + currentCategory);
 
     return (
         <main id="userHome">
             <aside>
                 <ul>
                     {/* This is the way to map through things to create elements!! */}
-                    {categories.map((category, index) => (
-                        <li className="categoryButton" key={index}>{category}</li>
+                    {categories.map((category) => (
+                        <li className="categoryButton" onClick={() => {console.log("Clicked Category: " + category); setCurrentCategory(category)}}>{category}</li>
                     ))}
                     {/* <li className="categoryButton">Breakfast</li> */}
                     {/* <li className="categoryButton">Lunch</li> */}
@@ -29,9 +40,24 @@ export default function UserHome() {
 
                 <p>No recipes found for this category. Choose another category or create a new recipe!</p>
 
-                <article>
+                {
+                    //Triple EQUALS SIGN!!! REMEMBER TO DO IT!! Or else.... things will NOT work
+                    !recipes.category === currentCategory ? <p>No recipes for this category. Create a new recipe or choose another category!</p> :
+                    
+                    recipes.filter(recipe => recipe.category === currentCategory)
+                        .map((recipe) => (
+                            <article>
+                                <h3>{recipe.name}</h3>
+                                <p>{recipe.description}</p>
+
+                                <a href="" id={recipe.recipeID}>View Recipe</a>
+                            </article>
+                        ))
+                }
+
+                {/* <article>
                     Here is where a recipe will go
-                </article>
+                </article> */}
             </section>
         </main>
     )
