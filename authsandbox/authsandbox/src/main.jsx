24ee218a92cb062c21a2f1071f5651f8b7665eae
samvrny <samvrny@gmail.com>
2025-04-15 18:32:15 -0500

    created ANOTHER blank vite/react application as a sandbox. Looking to continue on with my journey of testing out authorization functionality.

--- authsandbox/authsandbox/src/main.jsx
+++ authsandbox/authsandbox/src/main.jsx
@@ -12,7 +12,6 @@ import { Authenticator } from '@aws-amplify/ui-react'
 
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports'; // ✅ adjust path if needed
-
 Amplify.configure(awsExports);
 
 //IMPORT THE BROWSERROUTER
