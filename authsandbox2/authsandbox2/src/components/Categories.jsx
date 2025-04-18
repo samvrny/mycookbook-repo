f72eb759eef839278e80b4c2df1dd129b508da6b
samvrny <samvrny@gmail.com>
2025-04-18 17:12:18 -0500

    added the categories page - bare bones to the sandbox

--- authsandbox2/authsandbox2/src/components/Categories.jsx
+++ authsandbox2/authsandbox2/src/components/Categories.jsx
@@ -1,7 +1,31 @@
+import mockCategories from "../mockRecipeData/mockRecipes.json"
+
 export default function Categories() {
 
+    const categories = mockCategories.categories;
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log("Hello, Category");
+    }
+
     return (
-        <main></main>
+        <main id="categoriesMainElement">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="categoryInput">Add New Category</label>
+                <input type="text" id="categoryInput"/>
+                <input type="submit" value="Add Category"/>
+            </form>
+
+            <section>
+                <h2>Your Categories</h2>
+
+                {categories.map(category => {
+                    return <p>{category}</p>
+                })}
+            </section>
+        </main>
     )
 
 }
