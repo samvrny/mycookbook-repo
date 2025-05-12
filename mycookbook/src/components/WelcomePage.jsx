d8db737872865012ad9d529b75456919d845364c
samvrny <samvrny@gmail.com>
2025-05-11 20:13:49 -0500

    made some updates to the welcome page... words. Used Gemini to refactor the welcome page messages.

--- mycookbook/src/components/WelcomePage.jsx
+++ mycookbook/src/components/WelcomePage.jsx
@@ -14,9 +14,11 @@ export default function WelcomePage() {
             <section className="welcomePageCta">
                 <h1>Welcome To MyCookbook!</h1>
 
-                <p>Hello and welcome! Were happy you are here. MyCookbook is your one stop digital solution for all your recipe saving needs. Tired of looking for a recipe you found on the web and love, but can't seem to find again? Record it and save it here! Or maybe you don't want to have to wait for all the ads to load over and over on a recipe website. Perhaps you have dozens of old (and valuable!) paper recipes written down, but want a digital copy to back them up. Or perhaps you just in general want a good place to record all your regularly used recipes in a single easy to use place. Well look no further, we are here to help!</p>
+                <p>Frustrated by lost online recipes or endless website ads? What about those precious handwritten recipes you want to keep safe digitally?</p>
+                
+                <p>MyCookbook offers a simple and elegant solution. Finally, you can effortlessly save, organize, and access all your favorite recipes in one ad-free place. From online discoveries to cherished family traditions, keep your entire culinary collection together.</p>
 
-                <p>MyCookbook is free to use. Simply sign up and start saving recipes right away!</p>
+                <p>Ready to simplify your cooking? MyCookbook is free to use. Sign up quickly and start building your personalized digital cookbook today!</p>
 
                 <Link to="/sign-up" className="defaultButton buttonBlue welcomePageCtaButton">Sign Up / Log In</Link>
             </section>
@@ -26,7 +28,7 @@ export default function WelcomePage() {
                     <img src="../../homecard.jpg" alt="Hero Image" />
                     <div className="welcomePadding">
                         <h2>Share With Friends</h2>
-                        <p>Mycookbook allows you to print recipes or post and share them on social media, so you can share recipes with freinds and families in multiple formats. A paper copy of the family brownie recipe for your grandma, and an IM of it for your younger brother.</p>
+                        <p>Soon, MyCookbook will let you easily print your favorite recipes or share them directly on social media. Imagine quickly sending Grandma that beloved brownie recipe as a printed card, and instantly messaging it to your brother. Sharing your culinary joy in any format will be a breeze!</p>
                     </div>
                 </article>
 
@@ -34,7 +36,7 @@ export default function WelcomePage() {
                     <img src="../../homecard2.jpg" alt="Hero Image" />
                     <div className="welcomePadding">
                         <h2>Save Time</h2>
-                        <p>Don't waste time sorting through browser bookmarks or waiting for recipe pages with loads of ads to load. And reading 5 mile long stories before even getting to the recipe? Who has the time. Once you find a recipe you love, save it here for easy access.</p>
+                        <p>Stop wasting minutes sifting through endless browser bookmarks or waiting for ad-heavy recipe websites to load. Tired of scrolling through lengthy narratives just to get to the ingredients? Once you discover a recipe you love, save it instantly in MyCookbook for quick and easy access, every time.</p>
                     </div>
                 </article>
 
@@ -42,7 +44,7 @@ export default function WelcomePage() {
                     <img src="../../homecard3.jpg" alt="Hero Image" />
                     <div className="welcomePadding">
                         <h2>Protect Recipes You Love</h2>
-                        <p>We can understand the sentimental and human value of those old recipes your grandmother has written on an old index card. Save those recipes here too, in case it happens to be lost so not to lose treasured history!</p>
+                        <p>We understand the irreplaceable value of those handwritten recipes passed down through generations. Safeguard those culinary heirlooms right here in MyCookbook. By creating a digital backup, you ensure that those precious memories and flavors are protected from loss for years to come.</p>
                     </div>
                 </article>
             </section>
